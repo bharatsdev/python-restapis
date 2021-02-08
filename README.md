58dbf82d15358fb658017e6eb0531880cdd14116
everythingisdata <bharatmca2010@gmail.com>
2021-02-08 15:19:47 +0530

    Custom user management, Test and linting

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Python Rest APIs
 Python Django Rest APIs development
+
+
+## Commands 
+
+Run Test and Flake8 linting 
+   
+    docker-compose run app sh -c  "python manage.py test && flake8"
---
+++ app/app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test Creating an user with an email successful.."""
+        email = "test@mydata.com"
+        password = "password"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(email, user.email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for the new user is normalized"""
+        email = "test.@EVERYTHINGISDATA.com"
+        user = get_user_model().objects.create_user(email, "password123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises errors"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "123post")
+
+    def test_create_new_superuser(self):
+        """Test Creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            "bharatqq@every.com",
+            'test'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
