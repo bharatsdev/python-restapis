38a3fe6f52488da8973ac88c2ba866907c291622
everythingisdata <bharatmca2010@gmail.com>
2021-02-14 12:13:28 +0530

    build failing

--- app/app/settings.py
+++ app/app/settings.py
@@ -118,9 +118,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '/vol/web/media')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
