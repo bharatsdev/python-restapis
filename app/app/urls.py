dc03a3c4242e359891180a41122c9fa9e3a9640a
everythingisdata <bharatmca2010@gmail.com>
2021-02-14 09:08:47 +0530

    git add new feature for image upload

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/user/', include('users.urls')),
-                  path('api/recipe/', include('recipe.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('api/user/', include('users.urls')),
+    path('api/recipe/', include('recipe.urls')),
+]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,11 @@
 from core import models
-from recipe.serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailsSerializer
-from rest_framework import mixins, viewsets
+from recipe.serializers import TagSerializer, IngredientSerializer, RecipeSerializer, RecipeDetailsSerializer, \
+    RecipeImageSerializer
+from rest_framework import mixins, viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class BaseRecipeViewSets(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -60,10 +63,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return appropriate action class"""
+        print(f'Action of application {self.action}')
         if self.action is 'retrieve':
             return RecipeDetailsSerializer
+        elif self.action == 'upload_image':
+            return RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload a image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
